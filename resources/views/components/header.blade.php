38957e9a3cd296da595b6b9a0985623ab6da0252
Liana <145667748+liana1402@users.noreply.github.com>
2024-11-08 10:57:56 +0000

    start laravel

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,13 @@
+<div class="page-header">
+    <div class="flex-horizontal gap-16">
+        <h1>Atlas Books</h1>
+        <nav class="gap-8">
+            <a href="">Catalogue</a>
+            <a href="/account">Account</a>
+        </nav>
+    </div>
+    <div class="header-buttons">
+        <button id="login-button">Log In</button>
+        <button id="signup-button">Sign Up</button>
+    </div>
+</div>
