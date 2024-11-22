a43fd1cf653546dd9435f1c66a401c108818eda5
tedbeswick <c3016004@hallam.shu.ac.uk>
2024-11-22 09:41:56 +0000

    dashboard, book, catalogue pages look good now

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,7 @@
 <div class="page-header">
     <div class="flex-horizontal gap-16">
         <h1>Atlas Books</h1>
-        <nav class="gap-8">
+        <nav class="gap-32">
             <a href="/">Catalogue</a>
             <a href="/account">Account</a>
         </nav>
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Dashboard</title>
+        <link rel="stylesheet" href="{{ asset('styles.css') }}?ts=<?=time()?>" />
+    </head>
+
+<body class = "non-gradient-body">
+    <x-header></x-header>
+    <div class="dashboard-container">
+        <div class="recent-purchases"></div>
+        <div class="revenue"></div>
+        <div class="pie-chart"></div>
+        <div class="line-graph"></div>
+    </div>
+</body>
+</html>
