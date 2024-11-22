22274f2d50369e643353c9539c43dffaae3e8783
Artemis <simetra@simetra.dev>
2024-11-22 10:17:32 +0000

    Add catalogue page

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BOOK</title>
+    <title>Book</title>
     <link rel="stylesheet" href="{{ asset('styles.css') }}?ts=<?=time()?>" />
 </head>
 
