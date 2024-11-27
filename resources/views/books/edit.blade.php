f4a20522b6ac49494dbd4f01c8a94db3862347cd
tedbeswick <c3016004@hallam.shu.ac.uk>
2024-11-27 16:54:07 +0000

    edit book page css

--- resources/views/books/edit.blade.php
+++ resources/views/books/edit.blade.php
@@ -8,41 +8,52 @@
     <link rel="stylesheet" href="{{ asset('styles.css') }}"/>
 </head>
 
-<body>
+<body class="non-gradient-body">
 <x-header></x-header>
-<h1>Edit book</h1>
-<form method="POST" action="/books/{{$book["id"]}}" autocomplete="off">
-    @csrf
-    @method("PATCH")
 
-    <label for="id">Id</label>
-    <input readonly value="{{ $book["id"] }}" type="text" name="id"><br>
+<div class = "container-centre">
+    <div class="book-cover">
+        <img src="{{ $book["cover_url"] }}" alt="book cover">
+    </div>
+    <div class="edit-book-container">
 
+        <h1>Edit book</h1>
+            <form method="POST" action="/books/{{$book["id"]}}" autocomplete="off">
+                @csrf
+                @method("PATCH")
 
-    <label for="title">Title</label>
-    <input value="{{ $book["title"] }}" type="text" name="title"><br>
-
-
-    <label for="author">Author</label>
-    <input value="{{ $book["author"] }}" type="text" name="author"><br>
-
-
-    <label for="cover_url">Cover</label>
-    <input value="{{ $book["cover_url"] }}" type="text" name="cover_url"><br>
-
-
-    <label for="description">Description</label>
-    <input value="{{ $book["description"] }}" type="text" name="description"><br>
-
-
-    <label for="price">Price</label>
-    <input value={{$book["price"]}} type="number" min="1" step="any" name="price"><br>
-
-    <label for="quantity">Quantity</label>
-    <input value={{$book["quantity"]}} type="number" min="0" name="quantity"><br>
-
-    <button type="submit">Submit</button>
-</form>
+                <div class = "edit-book-input-field">
+                    <label for="id">Id</label>
+                    <input readonly value="{{ $book["id"] }}" type="text" name="id" disabled>
+                </div>
+                <div class = "edit-book-input-field">
+                    <label for="title">Title:</label>
+                    <input value="{{ $book["title"] }}" type="text" name="title">
+                </div>
+                <div class = "edit-book-input-field">
+                    <label for="author">Author:</label>
+                    <input value="{{ $book["author"] }}" type="text" name="author">
+                </div>
+                <div class = "edit-book-input-field">
+                    <label for="cover_url">Cover:</label>
+                    <input value="{{ $book["cover_url"] }}" type="text" name="cover_url">
+                </div>
+                <div class = "edit-book-input-field">
+                    <label for="description">Description:</label>
+                    <input value="{{ $book["description"] }}" type="text" name="description">
+                </div>
+                <div class = "edit-book-input-field">
+                    <label for="price">Price (£):</label>
+                    <input value={{$book["price"]}} type="number" min="1" step="any" name="price">
+                </div>
+                <div class = "edit-book-input-field">
+                    <label for="quantity">Quantity:</label>
+                    <input value={{$book["quantity"]}} type="number" min="0" name="quantity">
+                </div>
+                    <button id="edit-button">Submit</button>
+        </form>
+    </div>
+</div>
 </body>
 
 </html>
