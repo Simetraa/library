4e5d586a182c4c2fb3d8696271b5b55f420e1366
Liana <145667748+liana1402@users.noreply.github.com>
2024-12-06 13:11:26 +0000

    Generate random categories from list in BookFactory

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,11 +17,20 @@ class BookFactory extends Factory
      */
     public function definition(): array
     {
+        $categories = app(CategoryController::class)->getCategories();
+
+        $fakeCategories = [];
+
+        for ($i = 0; $i < random_int(0, 5); $i++) {
+            $fakeSubject = fake()->randomElement($categories);
+            $fakeCategories[] = fake()->randomElement($fakeSubject);
+        }
+
         return [
             'title' => fake()->sentence(),
             'author' => fake()->name(),
             'cover_url' => "https://m.media-amazon.com/images/I/81-VVvqF2UL._AC_UF894,1000_QL80_.jpg",
-            'subjects' => fake()->words(5),
+            'subjects' => $fakeCategories,
             'description' => fake()->paragraph(),
             'price' => fake()->numberBetween(5, 20),
             'quantity' => fake()->numberBetween(0, 10),
