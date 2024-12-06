e6e59a5862df003b699ad56a7dff25911d07cbd7
Liana <145667748+liana1402@users.noreply.github.com>
2024-12-06 13:12:20 +0000

    Fix spelling of Mathematics

--- resources/views/components/book.blade.php
+++ resources/views/components/book.blade.php
@@ -1,14 +1,14 @@
 @props(["book"])
-<a class="item-link" href="books/{{ $book["id"] }}">
+<a class="item-link" href="books/{{ $book->id }}">
     <div class="item-card">
-        <img src="{{ $book["cover_url"] }}" alt="book cover">
-        <h3>{{ $book["title"] }}</h3>
-        <h4>{{ $book["author"] }}</h4>
+        <img src="{{ $book->cover_url }}" alt="book cover">
+        <h3>{{ $book->title }}</h3>
+        <h4>{{ $book->author }}</h4>
 
         <div class="flex-horizontal-spaced">
-        @if($book["quantity"] == 0)
+        @if($book->quantity == 0)
             <span>Stock: 🔴</span>
-        @elseif($book["quantity"] >= 10)
+        @elseif($book->quantity >= 10)
             <span>Stock: 🟡</span>
         @else
              <span>Stock: 🟢</span>
