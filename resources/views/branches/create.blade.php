14772a82eb88a9e66f96a170f6d989eff18ef807
alex-be2 <alexbeddus@gmail.com>
2024-12-27 19:48:25 +0000

    css-> branches,create branch mobile -> account,reservation

--- resources/views/branches/create.blade.php
+++ resources/views/branches/create.blade.php
@@ -7,23 +7,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inventory</title>
     <link rel="stylesheet" href="{{ asset('styles.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('mobile.css') }}?ts=<?=time()?>" media ="only screen and (max-width: 720px)"/>
+
 </head>
 
 <body class="non-gradient-body">
 <x-header></x-header>
-<div class="flex-horizontal">
-    <h1>Create Branch</h1>
-</div>
-<div>
-    <form method="POST" action="/branches" autocomplete="off">
-        @csrf
+<div class="create-branch-container">
+    <div class="create-branch-card">
+        <h1>Create Branch</h1>
+        <div>
+            <form method="POST" action="/branches" autocomplete="off">
+                @csrf
 
-        <label for="name">Branch Name</label>
-        <input type="text" name="name" id="name" required>
-
-        <button type="submit">Submit</button>
-    </form>
+                <div class="branch-input-field">
+                    <label for="name">Branch Name:</label>
+                    <input type="text" name="name" id="name" required>
+                </div>
+                <button type="submit" class="edit-create-button-branch">Submit</button>
+            </form>
+        </div>
     </div>
+</div>
+
 </body>
 
 </html>
--- resources/views/branches/index.blade.php
+++ resources/views/branches/index.blade.php
@@ -11,26 +11,36 @@
 
 <body class="non-gradient-body">
 <x-header></x-header>
-<div class="header-row">
-    <h1>Branches</h1>
-    <a href="/branches/create" class="add-new-button">
+<div class="branch-container">
+    <div class="header-row">
+        <h1>Branches</h1>
+
+        <a href="/branches/create" class="add-new-button">
             <span class="material-symbols-outlined">
                 add
             </span>
             <span class="add-new-button-label">New</span>
-    </a>
-</div>
-<div>
-    @php
-        $branches = Branch::all()
-    @endphp
+        </a>
 
-    @foreach($branches as $branch)
-        <h2><a href="/branches/{{$branch->id}}">Branch</a></h2>
-        <p>Branch: {{ $branch->name }}</p>
-        <p>Id: {{ $branch->id }}</p>
-    @endforeach
+    </div>
+    <div>
+        @php
+            $branches = Branch::all()
+        @endphp
+
+        @foreach($branches as $branch)
+            <div class="branch-card">
+                <h2><a href="/branches/{{$branch->id}}">Branch</a></h2>
+                <p>Branch: {{ $branch->name }}</p>
+                <p>Id: {{ $branch->id }}</p>
+            </div>
+
+
+        @endforeach
+    </div>
 </div>
+
+
 
 </body>
 
